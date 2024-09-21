79454509e9a2b7f69344f5046eaa2649955ed97b
Arnsed44 <155181516+Arnsed44@users.noreply.github.com>
2024-09-20 21:00:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        section.classList.remove('active');
-    });
+document.getElementById("continue-btn").addEventListener("click", function() {
+    document.querySelector(".image-gallery").style.opacity = 0;
+    setTimeout(() => {
+        document.querySelector(".image-gallery").classList.add("hidden");
+        document.querySelector(".intro-section").style.opacity = 1;
+        document.querySelector(".intro-section").classList.remove("hidden");
+    }, 500);
+});
 
-    const activeSection = document.getElementById(sectionId);
-    activeSection.classList.add('active');
-}
-
-function toggleAnswer(answerId) {
-    const answerElement = document.getElementById(answerId);
-    answerElement.classList.toggle('show');
-}
+document.getElementById("about-btn").addEventListener("click", function() {
+    document.querySelector(".intro-section").style.opacity = 0;
+    setTimeout(() => {
+        document.querySelector(".intro-section").classList.add("hidden");
+        document.querySelector(".all-about-me").style.opacity = 1;
+        document.querySelector(".all-about-me").classList.remove("hidden");
+    }, 500);
+});
